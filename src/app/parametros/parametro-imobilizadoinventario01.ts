a0918437c6d166ba616e451d626df6626508258c
Falconii <falconi.2050@gmail.com>
2023-11-07 16:54:39 -0300

    Atualização B

---
+++ src/app/parametros/parametro-imobilizadoinventario01.ts
@@ -0,0 +1,10 @@
+export class ParametroImobilizadoinventario01{
+		public id_empresa:number = 0 ; 
+		public id_inventario:number = 0 ; 
+		public id_imobilizado:number = 0 ; 
+		public pagina:number = 0 ; 
+		public tamPagina:number = 50 ; 
+		public contador:string = 'N' ; 
+		public orderby:string = '' ; 
+		public sharp:boolean = false ; 
+}
